ad8e245b81a3948d5037b5cf9bfae615996395c3
heizeMochi <heizemochi@gmail.com>
2020-11-18 21:14:40 +0900

    Mothership Controll
    Mothership

---
+++ Assets/Scripts/MotherShip.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotherShip : MonoBehaviour
+{
+    public int line = 0;
+    public List<GameObject> Ypos;
+    public bool enemy;
+
+    void Update()
+    {
+        MotherShipInput();
+        transform.position = new Vector3(this.transform.position.x, Ypos[line].transform.position.y, 0);
+    }
+
+    void MotherShipInput()
+    {
+        if (!enemy)
+        {
+            bool Upkey = Input.GetKeyDown(KeyCode.UpArrow);
+            bool Downkey = Input.GetKeyDown(KeyCode.DownArrow);
+            if (Upkey && line < 2)
+            {
+                line++;
+            }
+            else if (Downkey && line > 0)
+            {
+                line--;
+            }
+        }
+    }
+}
