27154faab264f6f91f55565308dc851d1c737c12
heizeMochi <heizemochi@gmail.com>
2020-11-22 02:51:29 +0900

    select ui
    select ui

---
+++ Assets/Scripts/SelectUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectUI : MonoBehaviour
+{
+    public Mob[] mobs;
+    public Image[] MobIconObj;
+    public Sprite[] sprite;
+    public Text[] JewelryText;
+
+    private void Update()
+    {
+        for (int i = 0; i < JewelryText.Length; i++)
+        {
+            JewelryText[i].text = mobs[i].stat.Jewelry.ToString();
+        }
+    }
+}
