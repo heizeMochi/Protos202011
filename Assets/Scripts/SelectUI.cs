795e77865bdbe355ac6845ac1f483794ab749e16
heizeMochi <heizemochi@gmail.com>
2020-11-22 02:59:24 +0900

    q
    q

--- Assets/Scripts/SelectUI.cs
+++ Assets/Scripts/SelectUI.cs
@@ -7,14 +7,33 @@ public class SelectUI : MonoBehaviour
 {
     public Mob[] mobs;
     public Image[] MobIconObj;
-    public Sprite[] sprite;
+    public Sprite[] sprites;
     public Text[] JewelryText;
+
+    private void Start()
+    {
+        for (int i = 0; i < MobIconObj.Length; i++)
+        {
+            MobIconObj[i].sprite = sprites[i];
+        }
+    }
 
     private void Update()
     {
         for (int i = 0; i < JewelryText.Length; i++)
         {
             JewelryText[i].text = mobs[i].stat.Jewelry.ToString();
+        }
+        for (int i = 0; i < MobIconObj.Length; i++)
+        {
+            if (mobs[i].stat.Jewelry > GameManager.jewelry)
+            {
+                MobIconObj[i].color = Color.black;
+            }
+            else
+            {
+                MobIconObj[i].color = Color.white;
+            }
         }
     }
 }
